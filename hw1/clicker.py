ff9f5a7d88b2123191afd355159e62a6d0caa0f6
Jeff Zhou <jeffzhou63@uchicago.edu>
2025-01-12 17:41:05 -0600

    HW1

--- hw1/clicker.py
+++ hw1/clicker.py
@@ -53,7 +53,6 @@ You will find additional comments below that explain what the code does.
 """
 
 import sys
-
 import pygame
 
 # In our example, we will be alternating between red, blue, and green
@@ -78,25 +77,23 @@ class Clicker:
 
         # List to store the positions of circles
         self._circles = []
+        self.rad = 3
+        self.color = (255, 0, 0)  # Start with red color
 
         # Initialize all imported Pygame modules
-        # We must include this in any PyGame application
         pygame.init()
 
         # Set the title of the display window
         pygame.display.set_caption("Clicker")
 
-        # Create a window of size 600x600. This is referred to as the "surface"
-        # of our application (sometimes also known as our "canvas"). We will be
-        # able to refer to specific coordinates of this surface using (x,y)
-        # coordinates, where the top-left corner of the window is coordinate
-        # (0, 0). Note that the y coordinates grow *down* not up. For example,
-        # the bottom-left corner of the window would be (0, 599)
+        # Create a window of size 600x600
         self.surface = pygame.display.set_mode((600, 600))
 
-        # Create a clock object. This will later allow us to control how
-        # often our window is redrawn.
+        # Create a clock object
         self.clock = pygame.time.Clock()
+
+        # Flag to control visibility of the help message
+        self.show_help = False  # **Added this line**
 
         # Begin the event loop
         self.run_event_loop()
@@ -105,96 +102,59 @@ class Clicker:
         """
         The event loop for our application. Inside this loop, we will
         repeatedly perform the following operations:
-
             1. Check for new events
             2. If there are any new events, process the events.
             3. Re-draw the window
         """
         while True:
-            # Retrieve the latest events. More specifically, any time
-            # events happen, PyGame places them on a queue (to await
-            # processing). The pygame.event.get() method retrieves any
-            # events that have been placed on that queue.
-            #
-            # Individual events are represented as Event objects. You
-            # can read more about this here:
-            # https://www.pygame.org/docs/ref/event.html
             events = pygame.event.get()
-
-            # Process each event
             for event in events:
-                # In this application, we only process two events: the QUIT
-                # event (which happens when the user closes the window) and the
-                # MOUSEBUTTONUP (which happens when a mouse button is released,
-                # i.e., when "the pressed mouse button goes back up")
-
                 if event.type == pygame.QUIT:
-                    # When the close button of the window is clicked, we...
-
-                    # Uninitialize all Pygame modules
                     pygame.quit()
-
-                    # And terminate the program
                     sys.exit()
-
                 elif event.type == pygame.MOUSEBUTTONUP:
-
-                    # When the mouse button is released, add a new circle
-                    # In the MOUSEBUTTONUP event, the Event object will include
-                    # a "pos" attribute telling us the position of the surface
-                    # that was clicked.
-                    #
-                    # Notice how all we do is add a circle to the list of
-                    # circles. We don't actually draw anything on the surface
-                    # at this point.
                     self._circles.append(event.pos)
-
-                    # Uncomment the following line to see the exact positions
-                    # that are being clicked on the window.
-
-                    # print(f"Mouse clicked at position {event.pos}")
-
-            # Once we're done processing events, we redraw the window:
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.rad += 1  # Increase radius
+                        self.draw_window()
+                    elif event.key == pygame.K_LEFT:
+                        if self.rad > 1:
+                            self.rad -= 1  # Decrease radius but not below 1
+                        self.draw_window()
+                    elif event.key == pygame.K_UP:
+                        # Cycle through colors
+                        self.color = CIRCLE_COLORS[(CIRCLE_COLORS.index(self.color) + 1) % len(CIRCLE_COLORS)]
+                        self.draw_window()
+                    elif event.key == pygame.K_ESCAPE:
+                        self.rad = 3
+                        self.color = (255, 0, 0)
+                        self._circles.clear()  # Clear all circles
+                        self.show_help = False  # **Reset help message visibility**
+                        self.draw_window()
+                    elif event.key == pygame.K_h:
+                        self.show_help = not self.show_help
+                        self.draw_window()
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit(0)
             self.draw_window()
-
-            # The following ensures the surface is refreshed at 24 frames per
-            # second. If we omit this, the surface will be refreshed constantly
-            # (which will typically result in the application running slowly,
-            # because it's spending all its time constantly redrawing the
-            # window)
             self.clock.tick(24)
 
     def draw_window(self) -> None:
         """
         Clears the window and redraws all circles. This method separates the
         rendering (drawing) from the application logic and event processing.
-
-        Remember that we have a 600x600 surface to draw on. While we can
-        manipulate individual pixels, PyGame provides many convenience
-        functions to draw lines, circles, etc. You can find many of them here:
-        https://www.pygame.org/docs/ref/draw.html
         """
-
-        # Fill the surface with a grey color
-        # (128, 128, 128) is an RGB (Red Green Blue) color code,
-        # specifying the amount of each color on a scale from 0 to 255.
         self.surface.fill((128, 128, 128))
-
-        # For each circle in our list of circles, draw the circle.
         for i, circle in enumerate(self._circles):
-            # Alternate circle colors between red, green, and blue
             color = CIRCLE_COLORS[i % 3]
-
-            # Use pygame.draw.circle to draw a circle
             pygame.draw.circle(self.surface, color=color,
-                               center=circle, radius=20)
-
-        # Instruct PyGame to actually refresh the window with
-        # the elements we have just drawn
+                               center=circle, radius=self.rad)
+        if self.show_help:
+            font = pygame.font.SysFont("Arial", 24)
+            help_text = font.render("Use arrows to resize, ESC to reset, H for help, Q to quit", True, (255, 255, 255))
+            self.surface.blit(help_text, (10, 10))
         pygame.display.update()
-
-
-# If you're unsure about what the following lines mean, make sure to review
-# this: https://book.cs-apps.org/getting_started/organization/index.html
 if __name__ == "__main__":
     Clicker()
